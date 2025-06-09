64334eb70559dd2cc23d9d86e61e2bcbd622520e
Tom Bieber <122181710+t-bieber@users.noreply.github.com>
2025-06-09 15:14:50 +0200

    Fix package imports

--- control/python_pc/gui/gui.py
+++ control/python_pc/gui/gui.py
@@ -1,7 +1,11 @@
 # gui.py
 import sys
 from PyQt5.QtWidgets import QApplication
-from main_window import MainWindow
+
+# Import from the current package so this module works whether executed as part
+# of the package or as a script. Absolute imports would fail when invoked via
+# ``control/python_pc/main.py`` as they would not resolve correctly.
+from .main_window import MainWindow
 from utils.settings_manager import SettingsManager
 
 def run_gui():
